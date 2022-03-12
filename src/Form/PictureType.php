101f331f30eaa28c43a1a5af826eb7fdf3650829
Taeco <97479672+ta-echo@users.noreply.github.com>
2022-03-12 12:46:24 +0100

    Create PictureType.php

---
+++ src/Form/PictureType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Picture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class PictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('name', FileType::class,
+        [   
+            'label' =>'Chargez une image',
+            'data_class' => null,
+            'required' => false
+        ]) 
+            ->add('alt')
+        
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Picture::class,
+        ]);
+    }
+}
